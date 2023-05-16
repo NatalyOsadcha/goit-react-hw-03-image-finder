c4b2b7b26325df57fd4e9f519db64ca98b1a0368
NatalyOsadcha <114231206+NatalyOsadcha@users.noreply.github.com>
2023-05-17 01:34:24 +0300

    fixed pages updating

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,102 +1,24 @@
 import React, { Component } from 'react';
-import { getSearchImage } from 'components/Api/getSearchImage';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
-import { BallTriangle } from 'react-loader-spinner';
-import Button from '../Button/Button';
 import css from './ImageGallery.module.css';
-import Alert from '@mui/material/Alert';
 import PropTypes from 'prop-types';
 
 export default class ImageGallery extends Component {
-  state = {
-    hits: null,
-    totalHits: null,
-    loading: false,
-    error: '',
-    page: 1,
-  };
-
-  handleLoadMore = ({ page }) => {
-    this.setState({ page });
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-    const prevPage = prevState.page;
-    const nextPage = this.state.page;
-    const prevImage = prevProps.searchImage;
-    const nextImage = this.props.searchImage.trim();
-
-    if (prevImage !== nextImage && nextImage) {
-      this.setState({ loading: true, hits: [], error: null });
-      getSearchImage(nextImage, nextPage)
-        .then(data => {
-          if (data.hits && data.totalHits)
-            return this.setState({
-              hits: data.hits,
-              totalHits: data.totalHits,
-            });
-          return Promise.reject(data.message);
-        })
-        .catch(error => {
-          this.setState({ error });
-        })
-        .finally(() => this.setState({ loading: false }));
-    }
-
-    if (nextPage !== prevPage) {
-      this.setState({ loading: true });
-      getSearchImage(nextImage, nextPage)
-        .then(data => {
-          this.setState(prevState => ({
-            hits: [...prevState.hits, ...data.hits],
-          }));
-        })
-        .finally(() => this.setState({ loading: false }));
-    }
-  }
-
-  hideButton = () => {
-    const page = this.state.page;
-    const totalHits = this.state.totalHits;
-    const finalPage = Math.ceil(Number(this.state.totalHits / 12));
-    if (page === finalPage || totalHits < 13) {
-      return 'none';
-    }
-    return 'block';
-  };
 
   render() {
-    // const nextImage = this.props.searchImage.trim();
-    const { hits, loading, error, page } = this.state;
+    const {hits} = this.props;
     return (
       <>
-        {loading && <BallTriangle color="#4b5cdd" />}
-        {error && (<Alert severity="error">Oops, something goes wrong</Alert>)}
-        {/* {nextImage === '' && (<Alert severity="warning">Enter something for searching images</Alert>)} */}
-        {hits && hits.length > 0 && (
           <ul className={css.imageGallery}>
             {hits.map(hit => (
               <ImageGalleryItem hit={hit} />
             ))}
           </ul>
-        )}
-        {hits && hits.length === 0 && (<Alert severity="warning">Nothing found for your request</Alert>)}
-        {
-          <Button
-            onClick={this.handleLoadMore}
-            page={page}
-            hideButton={this.hideButton}
-          />
-        }
       </>
     );
   }
 }
 
 ImageGallery.propTypes = {
-  totalHits: PropTypes.number,
-  loading: PropTypes.bool,
-  error: PropTypes.string,
   hits: PropTypes.arrayOf(PropTypes.object),
-  page: PropTypes.number,
 };
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -2,21 +2,23 @@ import React, { Component } from 'react';
 import Modal from '../Modal/Modal';
 import css from './ImageGalleryItem.module.css';
 import PropTypes from 'prop-types';
+import { nanoid } from 'nanoid';
 
 export default class ImageGalleryItem extends Component {
   state = {
     showModal: false,
   };
+  imgId = nanoid();
 
   toggleModal = () => {
     this.setState(state => ({ showModal: !state.showModal }));
   };
 
   render() {
-    const { index, webformatURL, tags } = this.props.hit;
+    const { webformatURL, tags } = this.props.hit;
     return (
       <>
-        <li key={index + 1} className={css.imageGalleryItemImage}>
+        <li key={this.imgId} className={css.imageGalleryItemImage}>
           <img src={webformatURL} alt={tags} onClick={this.toggleModal} />
         </li>
         {this.state.showModal && (
